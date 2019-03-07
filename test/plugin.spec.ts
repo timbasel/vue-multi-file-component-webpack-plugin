fa2ac1fd070b9f3354478cc322ff6acde948cd36
timbasel <tim@basel-berlin.de>
2019-03-07 16:08:06 +0100

    Initialize testing & linting tools

---
+++ test/plugin.spec.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+
+describe("Multi-File Component Webpack Plugin", () => {
+  describe("Construction", () => {
+
+  })
+
+  describe("Application", () => {
+
+  });
+
+  describe("Error Handling", () => {
+  });
+});
