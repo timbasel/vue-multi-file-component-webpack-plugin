00a5b6f4cf22bd44637b4f6df06dc9740339ec76
timbasel <tim@basel-berlin.de>
2019-03-11 09:58:54 +0100

    Implement webpack plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,24 +1,61 @@
 import { OptionObject } from "loader-utils";
-import { Compiler, loader } from "webpack";
+import { isRegExp } from "util";
+import { Compiler, Loader } from "webpack";
+
+
+const pluginName = "VueMultiFileComponentPlugin";
+const defaultOptions: OptionObject = {
+  test: /\.vue\./,
+};
+
 
 class VueMultiFileComponentPlugin {
-  private readonly defaultOptions: OptionObject = {
-    test: /\.vue\./,
-  };
-
   private options: OptionObject;
 
   constructor(options?: OptionObject) {
-    this.options = Object.assign({}, this.defaultOptions, options);
-    this.options;
+    this.options = Object.assign({}, defaultOptions, options);
+
+    if (!isRegExp(this.options.test)) {
+      throw new Error(`[${pluginName}] option 'test' must be of RegExp type`);
+    }
   }
 
-  public static loader(this: loader.LoaderContext, source: string): string {
-    return source;
+  public static loader(options: OptionObject): Loader {
+    return { loader: require.resolve("./loader"), options };
   }
 
   public apply(compiler: Compiler) {
+    const plugins = compiler.options.plugins;
+    if (!plugins) {
+      throw new TypeError(`[${pluginName}] no plugin configuration found`);
+    }
 
+    const pluginIndex = plugins.findIndex((plugin) => {
+      return plugin.constructor.name === pluginName;
+    });
+    const vueLoaderPluginIndex = plugins.findIndex((plugin) => {
+      return plugin.constructor.name === "VueLoaderPlugin";
+    });
+
+    // ensure VueMultiFileComponentPlugin is used after the VueLoaderPlugin
+    if (vueLoaderPluginIndex === -1 || vueLoaderPluginIndex > pluginIndex) {
+      throw new Error(`[${pluginName}] ${pluginName} must be used after VueLoaderPlugin`);
+    }
+
+    if (!compiler.options.module) {
+      throw new TypeError(`[${pluginName}] no module configuration found`);
+    }
+
+    // create webpack loader rule ()
+    const rule = {
+      test: this.options.test,
+      use: (options: OptionObject) => {
+        return /separated/.test(options.resourceQuery) ?
+          [] : [ { loader: "vue-loader" }, VueMultiFileComponentPlugin.loader(this.options) ];
+      },
+    };
+
+    compiler.options.module.rules.push(rule);
   }
 }
 
